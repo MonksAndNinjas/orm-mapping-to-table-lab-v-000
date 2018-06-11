d6591b2d8181d6a9b3180faf10e1bd035b47810b
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-11 21:33:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,13 @@ class Student
   end
 
   def self.create_table
-
+    sql = <<-sql
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
+        SQL
   end
 
 end
